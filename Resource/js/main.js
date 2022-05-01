6f2f77460a458e3608cc193ac868691af71f754e
Akash Raman <akashjayaraman8841@gmail.com>
2022-05-01 20:51:56 +0530

    main.js change26

--- Resource/js/main.js
+++ Resource/js/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const store = document.getElementById('name');
-
+const userNameList = document.querySelector('.user-name-list');
+const logoutList = document.querySelector('.logout-list');
+const modal = document.querySelector('.redirecting-modal');
+const redBox = document.querySelector('.red-box');
 // const guest = {
 //     name: 'Guest',
 //     email: 'unknown',
@@ -19,19 +22,43 @@ class database {
     #currentAccount;
     constructor(){
         this._getLocalStorage();
+        userNameList.addEventListener('mouseover', function(){
+            logoutList.classList.remove('hidden');
+        });
+        logoutList.addEventListener('mouseover', function(){
+            logoutList.classList.remove('hidden');
+        });
+        userNameList.addEventListener('mouseleave', function(){
+            logoutList.classList.add('hidden');
+        });
+        logoutList.addEventListener('mouseleave', function(){
+            logoutList.classList.add('hidden');
+        });
+        logoutList.addEventListener('click',this._logout)
     }
     
     _getLocalStorage(){
         const data = JSON.parse(localStorage.getItem('currentAccount'));
         if(!data){
-            const guest = JSON.parse(localStorage.getItem('guest'));
-            this.#currentAccount = guest;
-            store.insertAdjacentHTML('afterbegin',`${guest.firstName}`);
-            console.log(guest.name);
+            modal.classList.remove('hidden')
+            setTimeout(redBox.classList.remove('hidden'),500)
+            setTimeout(this._locationToLogOrSign,2000);
+            
+            // const guest = JSON.parse(localStorage.getItem('guest'));
+            // this.#currentAccount = guest;
+            // store.insertAdjacentHTML('afterbegin',`${guest.firstName}`);
+            // console.log(guest.name);
             return;
         }
         store.insertAdjacentHTML('afterbegin',`${data.firstName}`);
-        console.log(data.name);
+    }
+
+    _logout(){
+        localStorage.removeItem('currentAccount')
+        location.href = "https://akashraman.github.io/CodeAlgo/"
+    }
+    _locationToLogOrSign(){
+        location.href = "https://akashraman.github.io/CodeAlgo-LogOrSign/"
     }
 }
 
